93f70f534f227e20d10022a6e168d8f56eabfe7f
Caitlyn Daly <cnd9351@rit.edu>
2017-10-16 10:22:08 -0400

    added all pictures

--- assets/data.js
+++ assets/data.js
@@ -57,106 +57,106 @@ var object = {
     },
     'Sith Lord': {
         value: 'Palpatine, also known as Darth Sidious, was a Force-sensitive Human male who served as the last Supreme Chancellor of the Galactic Republic and the first Emperor of the Galactic Empire. A Dark Lord of the Sith in the Order of the Sith Lords, recorded by history as the most powerful who had ever lived, his entire life was the culmination of a thousand-year plan to overthrow the Republic and the Jedi Order from within.',
-        answers: ['images/sithlord.png']
+        answers: ['images/sithlord.jpg']
     },
     Chewbacca: {
         value: 'Chewbacca, known affectionately to his friends as Chewie, was a Wookiee warrior, smuggler, and resistance fighter who fought in the Clone Wars, the Galactic Civil War, and the conflict between the First Order and the Resistance. He hailed from the planet Kashyyyk and became a Wookiee military leader. During the Clone Wars, he was captured by Trandoshan slavers and held captive on Wasskah, but he worked with a fellow captive, Jedi Commander Ahsoka Tano, to escape. He later commanded Wookiee forces during the Battle of Kashyyyk alongside the Grand Army of the Republic, led by Jedi Master Yoda. During the battle, one of the last ones of the war, Yoda\'s clone troopers received Order 66 from Supreme Chancellor Palpatine and, with the help of Chewbacca, Yoda escaped Kashyyyk and the destruction of the Jedi Order.',
-        pics: ['images/chewbacca.png']
+        pics: ['images/chewbacca.jpg']
     },
     'Luke Skywalker': {
         value: 'Luke Skywalker was a human male Jedi Master who was instrumental in defeating the Galactic Empire and the Sith during the Galactic Civil War. He was the son of the redeemed Jedi Knight Anakin Skywalker and Senator Padmé Amidala, the grandson of Shmi Skywalker Lars, the grandson of both Ruwee Naberrie and Jobal Naberrie, the step-nephew of Owen and Beru Lars, the twin brother of Leia Organa and the uncle of Ben Solo. Luke and his sister were born in 19 BBY on the asteroid Polis Massa, unbeknownst to their father, the recently christened Sith Lord Darth Vader. After his mother died during labor, Luke was separated from his sister and taken by Jedi Master Obi-Wan Kenobi to the planet Tatooine to be raised by the Lars family, out of sight of the Galactic Empire.',
-        pics: ['images/luke.png']
+        pics: ['images/luke.jpg']
     },
     'Darth Vader': {
         value: 'Anakin Skywalker was a Force-sensitive human male who served the Galactic Republic as a Jedi Knight and later the Galactic Empire as the Sith Lord Darth Vader. Believed to have been conceived by the Force, he was born to Shmi Skywalker and came to reside on the Outer Rim planet of Tatooine as a small child, where he and his mother were slaves of the prominent crime lord Gardulla the Hutt before falling into the ownership of a local Toydarian junk dealer named Watto. An engineering prodigy, Skywalker rebuilt the protocol droid C-3PO using scavenged parts to help his mother. Soon thereafter, during the Naboo Crisis, he was discovered by Jedi Master Qui-Gon Jinn and his apprentice, Obi-Wan Kenobi, who freed Anakin from slavery after a successful wager on the youth\'s podracing skills. Bringing him into the Jedi Order, Jinn hailed Skywalker as the Chosen One of Jedi prophecy, destined to defeat the Sith and bring balance to the Force.',
-        pics: ['images/luke.png']
+        pics: ['images/vader.jpg']
     },
     Yoda: {
         value: 'Yoda, a male member of a mysterious species, was a revered Jedi Master who served as the Grand Master of the Jedi Order in the waning days of the Galactic Republic. He was renowned within the Order for his wisdom, powers of the Force, and lightsaber combat, and he lived for nearly 900 years. His time during the last days of the Jedi Order and beyond made him a consequential figure in galactic history.',
-        pics: ['images/yoda.png']
+        pics: ['images/yoda.jpg']
     },
     Lando: {
         value: 'Lando Calrissian was a human male smuggler, gambler, and card player who became Baron Administrator of Cloud City and, later, a general in the Rebel Alliance. He was the owner of the Millennium Falcon before losing it to Han Solo in a game of sabacc. After losing the Falcon, Calrissian put an end to his days as a smuggler and became an entrepreneur, setting up a small mining operation on the planet Lothal before eventually becoming the leader of Cloud City in the skies of the planet Bespin.',
-        pics: ['images/lando.png']
+        pics: ['images/lando.jpg']
     },
     'Princess Leia': {
         value: 'Leia Organa (born Leia Amidala Skywalker) was a Force-sensitive human female who served as princess of Alderaan, a member of the Imperial Senate, a leader in the Alliance to Restore the Republic and New Republic, and the founding general of the Resistance. Through it all, she became instrumental in the defeat of the Galactic Empire in the Galactic Civil War and led the fight against the First Order. She was born nineteen years before the Battle of Yavin as the daughter of Jedi Knight Anakin Skywalker and Senator Padmé Amidala. After her mother died and her father fell to the dark side of the Force to become Darth Vader, Leia was adopted by the House of Organa. She took the place of her adoptive father, Bail Organa, in the Senate and helped lead the fight against the Empire.',
-        pics: ['images/leia.png']
+        pics: ['images/leia.jpg']
     },
     'Han Solo': {
         value: 'Han Solo was a human male smuggler who became a leader in the Alliance to Restore the Republic and an instrumental figure in the defeat of the Galactic Empire during the Galactic Civil War. He hailed from Corellia and became a smuggler in the employ of Jabba the Hutt, even completing the Kessel Run in less than twelve parsecs with his prized ship, the Millennium Falcon.',
-        pics: ['images/han.png']
+        pics: ['images/han.jpg']
     },
     'Darth Maul': {
         value: 'Maul was a Dathomirian Zabrak male who lived during the waning days of the Galactic Republic and the reign of the Galactic Empire. The son of Mother Talzin of the Nightsisters, he was taken as the Sith apprentice of Darth Sidious, the Dark Lord of the Sith, and given the name Darth Maul. After revealing the existence of the Sith to the Jedi Order, Maul supposedly fell in a lightsaber duel against Obi-Wan Kenobi during the Battle of Naboo. Over a decade later, he returned from exile and formed the criminal army known as the Shadow Collective, intent on reclaiming the power he had lost. His quest for revenge against Kenobi and the Sith continued into the Imperial age, bringing him into conflict with the early rebellion against the Empire.',
-        pics: ['images/maul.png']
+        pics: ['images/maul.jpg']
     },
     'Count Dooku': {
         value: 'Dooku, a Force-sensitive human male, was a Sith Lord who led the Confederacy of Independent Systems as Head of State during the Clone Wars. Although he was once a Jedi Master, Dooku left the Jedi Order and the Galactic Republic after growing disillusioned with the corruption in the government. Turning from the light he once served as a Jedi, Dooku joined the dark side of the Force and became the apprentice of Darth Sidious, the Dark Lord of the Sith, taking on the name Darth Tyranus and reclaiming his title as the Count of Serenno.',
-        pics: ['images/dooku.png']
+        pics: ['images/dooku.jpg']
     },
     'Baby Anakin': {
         value: 'This was the only stage of Anakin that is tolerable.. nuff said',
-        pics: ['images/baby.png']
+        pics: ['images/baby.jpg']
     },
     'Qui-Gon': {
         value: 'Qui-Gon Jinn was a male human Jedi Master born on Coruscant during the declining days of the Galactic Republic. Trained by Count Dooku, Jinn was considered a maverick within the Jedi Order, one who did not always follow the Jedi Code; according to his Padawan, Obi-Wan Kenobi, Jinn could have been a member of the Jedi High Council if he had followed the Code. In the years prior to his death, Jinn began studying the secrets of eternal consciousness after death, having long been a student of the Living Force.',
-        pics: ['images/quigon.png']
+        pics: ['images/quigon.jpg']
     },
     'Padme': {
         value: 'Padmé Amidala Naberrie, commonly known as Padmé Amidala, was a human female politician who became a leading voice for peace and diplomacy during the final years of the Galactic Republic. Rising to galactic prominence after the Trade Federation invaded and occupied her homeworld of Naboo, she emerged as a leading member of the Galactic Senate during the Clone Wars. Through her furtive relationship and marriage to Jedi Knight Anakin Skywalker, she was the mother of the legendary Jedi Master Luke Skywalker and Princess Leia Organa of Alderaan, the latter of whom followed in her footsteps as a champion for the democratic ideals of the Republic.',
-        pics: ['images/padme.png']
+        pics: ['images/padme.jpg']
     },
     'Annoying Anakin': {
         value: 'The beginning of the end',
-        pics: ['images/annoying.png']
+        pics: ['images/annoying.jpg']
     },
     'Mace Windu': {
         value: 'Mace Windu, a Force-sensitive human male, was a revered Jedi Master and member of the Jedi High Council during the last years of the Galactic Republic. During his time in the Jedi Order, he once served as elected leader of the Jedi and, during the Clone Wars, as a Jedi General in the Grand Army of the Republic. He was the greatest champion of the Jedi Order and promoted its ancient traditions amidst the growing influence of the dark side of the Force in the corrupt, declining days of the Republic.',
-        pics: ['images/luke.png']
+        pics: ['images/mace.jpg']
     },
     'Obi-Wan': {
         value: 'You were the chosen one! It was said that you would destroy the Sith, not join them! Bring balance to the force, not leave it in darkness! You were my brother Anakin! I loved you!',
-        pics: ['images/obi.png']
+        pics: ['images/obi.jpg']
     },
     'Evil Anakin': {
         value: 'You underestimate my power!',
-        pics: ['images/evil.png']
+        pics: ['images/evil.jpg']
     },
     'Old Han': {
         value: 'Harrison Ford hated my character and thats why I had to die, sad, I know.',
-        pics: ['images/oldhan.png']
+        pics: ['images/oldhan.jpg']
     },
     'Old Leia': {
         value: 'Kinda useless honestly.',
-        pics: ['images/oldleia.png']
+        pics: ['images/oldleia.jpg']
     },
     'Finn': {
         value: 'Finn was a human male who served as a First Order stormtrooper, designation FN-2187 and known to his fellow soldiers as Eight-Seven. He was considered one of the group\'s best stormtroopers before he deserted to join the Resistance in their struggle against the First Order. Having left the First Order, he took on the name "Finn" at the suggestion of Resistance pilot Poe Dameron, based on his stormtrooper designation, having never been given an actual name. After leaving the First Order, he reluctantly became involved in the droid BB-8\'s quest to rejoin the Resistance, and escaped the desert planet Jakku with BB-8 and a female scavenger named Rey.',
-        pics: ['images/finn.png']
+        pics: ['images/finn.jpg']
     },
     'Rey': {
         value: 'Rey was a human female scavenger and Force prodigy who discovered her latent Force-sensitivity while on a journey to find the legendary Jedi Master Luke Skywalker and bring a new hope to a galaxy on the brink of war. Rey grew up alone in the wastes of the desert planet Jakku, hoping for the day her family would return to her while honing her skills as a scavenger and a warrior. Her life changed forever when she encountered BB-8, a Resistance astromech droid who carried information about Skywalker\'s whereabouts, and a stormtrooper named Finn who defected from the First Order. They were hunted by the First Order, forcing them to escape from Jakku aboard the Millennium Falcon, eventually finding help from Han Solo and Chewbacca during their journey to the Resistance base on D\'Qar.r',
-        pics: ['images/rey.png']
+        pics: ['images/rey.jpg']
     },
     'Old Luke': {
         value: 'Heres to hoping that he is not an unsufferable pussy in this movie.',
-        pics: ['images/oldluke.png']
+        pics: ['images/oldluke.jpg']
     },
     'Kylo Ren': {
         value: 'I have daddy issues.',
-        pics: ['images/kylo.png']
+        pics: ['images/kylo.jpg']
     },
     'Bruh': {
         value: 'Bruh',
-        pics: ['images/troll1.png']
+        pics: ['images/troll.jpg']
     },
     'out': {
         value: 'out',
-        pics: ['images/troll3.png']
+        pics: ['images/troll3.jpg']
     },
     'its not': {
         value: 'its not',
-        pics: ['images/troll2.png']
+        pics: ['images/troll2.jpg']
     }
 };
