ebccb0704df3a27388b005f1070d349b6d0b7b60
Caitlyn Daly <cnd9351@rit.edu>
2017-10-15 21:23:43 -0400

    working for first select

---
+++ data.js
@@ -0,0 +1,139 @@
+var object = {
+    starwars: {
+        value: 'Choose a trilogy',
+        answers: ['Original', 'George Lucas greatest failure 1-3 ', 'New 7-9']
+    },
+    Original: {
+        value: 'Choose an episode',
+        answers: ['A New Hope', 'Empire Strikes Back', 'Return of the Jedi']
+    },
+    'George Lucas greatest failure 1-3': {
+        value: 'Choose an episode',
+        answers: ['Phantom Menace', 'The Clone Wars', 'Revenge of the Sith']
+    },
+    'New 7-9': {
+        value: 'Choose an episode',
+        answers: ['The Force Awakens', 'The Last Jedi', 'Episode 9']
+    },
+    'A New Hope': {
+        value: 'Choose a character',
+        answers: ['Ken Kenobi', 'Luke Skywalker', 'Darth Vader']
+    },
+    'Empire Strikes Back': {
+        value: 'Choose a character',
+        answers: ['Yoda', 'Lando', 'Princess Leia']
+    },
+    'Return of the Jedi': {
+        value: 'Choose a character',
+        answers: ['Han Solo', 'Sith Lord', 'Chewbacca']
+    },
+    'Phantom Menace': {
+        value: 'Choose a character',
+        answers: ['Baby Anakin', 'Qui-Gon', 'Darth Maul']
+    },
+    'The Clone Wars': {
+        value: 'Choose a character',
+        answers: ['Count Doku', 'Padme', 'Annoying Anakin']
+    },
+    'Revenge of the Sith': {
+        value: 'Choose a character',
+        answers: ['Mace Windu', 'Obi-Wan', 'Evil Anakin']
+    },
+    'The Force Awakens': {
+        value: 'Choose a character',
+        answers: ['Old Han', 'Old Leia', 'Finn']
+    },
+    'The Last Jedi': {
+        value: 'Choose a character',
+        answers: ['Rey', 'Old Luke', 'Kylo Ren']
+    },
+    'Episode 9': {
+        value: 'Choose a character',
+        answers: ['Bruh', 'its not', 'out']
+    },
+    'Ken Kenobi': {
+        value: 'Choose a trilogy',
+        answers: ['Original', 'George Lucas greatest failure 1-3 ', 'New 7-9']
+    },
+    'Sith Lord': {
+        value: 'Choose an episode',
+        answers: ['A New Hope', 'Empire Strikes Back', 'Return of the Jedi']
+    },
+    Chewbacca: {
+        value: 'Choose an episode',
+        answers: ['Phantom Menace', 'The Clone Wars', 'Revenge of the Sith']
+    },
+    'Luke Skywalker': {
+        value: 'Choose an episode',
+        answers: ['The Force Awakens', 'The Last Jedi', 'Episode 9']
+    },
+    'Darth Vader': {
+        value: 'Choose a character'
+    },
+    Yoda: {
+        value: 'Choose a character'
+    },
+    Lando: {
+        value: 'Choose a character'
+    },
+    'Princess Leia': {
+        value: 'Choose a character'
+    },
+    'Han Solo': {
+        value: 'Choose a character'
+    },
+    'Darth Maul': {
+        value: 'Choose a character'
+    },
+    'Count Doku': {
+        value: 'Choose a character'
+    },
+    'Baby Anakin': {
+        value: 'Choose a character'
+    },
+    'Qui-Gon': {
+        value: 'Choose a character'
+    },
+    'Padme': {
+        value: 'Choose a character'
+    },
+    'Annoying Anakin': {
+        value: 'Choose a character'
+    },
+    'Mace Windu': {
+        value: 'Choose a character'
+    },
+    'Obi-Wan': {
+        value: 'Choose a character'
+    },
+    'Evil Anakin': {
+        value: 'Choose a character'
+    },
+    'Old Han': {
+        value: 'Choose a character'
+    },
+    'Old Leia': {
+        value: 'Choose a character'
+    },
+    'Finn': {
+        value: 'Choose a character'
+    },
+    'Rey': {
+        value: 'Choose a character'
+    },
+    'Old Luke': {
+        value: 'Choose a character'
+    },
+    'Kylo Ren': {
+        value: 'Choose a character'
+    },
+    'Bruh': {
+        value: 'Choose a character'
+    },
+    'out': {
+        value: 'Choose a character'
+    },
+    'its not': {
+        value: 'Choose a character'
+    }
+};
---
+++ script.js
@@ -0,0 +1,4 @@
+scr = "data.js";
+
+//save value for getting the element by id
+var list = document.getElementById('list');
